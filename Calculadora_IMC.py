729312fbe392d615c8cacfb3c5bb86fe79ff08b4
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-10-05 05:36:12 -0300

    Update Calculadora_IMC.py

--- Calculadora_IMC.py
+++ Calculadora_IMC.py
@@ -2,9 +2,11 @@ import time
 print("Bem vindo a calculadora de IMC.")
 print("Qual a sua altura?")
 altura = float(input())
+
 print("Qual o seu peso?")
 peso = float(input())
+
 print("Calculando.....")
 print("===============")
 time.sleep(1)
-print("Seu IMC é: {0:.4f}".format(peso / altura**2))
+print("Seu IMC é: {:.2f}".format(peso / altura**2))
